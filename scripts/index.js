aee01f4fcab1a84050e0d84a73a16336200feb9f
owenPatt <owen.patt.8@gmail.com>
2023-07-31 19:36:06 -0400

    made edit and close buttons work for profile modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,17 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+//Elements
+const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileModalCloseBtn = document.querySelector("#profile-modal-close-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+//Event Listeners
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileModalCloseBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
