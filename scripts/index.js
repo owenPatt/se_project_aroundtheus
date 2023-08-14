a3adcc7c8fcfc5675e776e7a7b1d65f98cac919d
owenPatt <owen.patt.8@gmail.com>
2023-08-13 22:39:22 -0400

    -Feat: Added picture modal once user clicks on card images

--- scripts/index.js
+++ scripts/index.js
@@ -32,7 +32,7 @@ const profileEditBtn = profile.querySelector("#profile-edit-button");
 const profileTitle = profile.querySelector("#profile-title");
 const profileDescription = profile.querySelector("#profile-description");
 
-//modal elements
+//profile modal elements
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileModalCloseBtn = profileEditModal.querySelector("#profile-modal-close-button");
 const profileModalSaveBtn = profileEditModal.querySelector("#profile-modal-save-button");
@@ -41,55 +41,106 @@ const profileModalDescriptionInput = profileEditModal.querySelector("#profile-mo
 
 //card elements
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
-const cardList = document.querySelector('.cards__list');
+const cardList = document.querySelector(".cards__list");
+const addCardBtn = document.querySelector("#add-card-button");
+
+//add card modal elements
+const addCardModal = document.querySelector("#card-add-modal");
+const cardModalCloseBtn = addCardModal.querySelector("#card-modal-close-button");
+const cardModalTitleInput = addCardModal.querySelector("#add-card-modal-title-input");
+const cardModalImageInput = addCardModal.querySelector("#add-card-modal-image-link-input");
+
+//Picture Modal elements
+const pictureModal = document.querySelector("#picture-modal");
+const pictureModalCloseBtn = pictureModal.querySelector("#picture-modal-close-button");
+const pictureModalImage = pictureModal.querySelector("#picture-modal-image");
+const pictureModalTitle = pictureModal.querySelector("#picture-modal-title");
 
 //***Functions***
-//Takes a modal and removes the modal_opened css class
 function closeModal(modal){
   modal.classList.remove("modal_opened");
 }
 
+function openModal(modal){
+  modal.classList.add("modal_opened");
+}
+
 //Grabs the card element and sets values equal to data given
 function getCardElement(data){
-  //grabs template and card elements
   const cardElement = cardTemplate.cloneNode(true);
-  const cardImageEl = cardElement.querySelector('.card__image');
-  const cardTitleEl = cardElement.querySelector('.card__title');
-  //sets image src and alt
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const likeBtn = cardElement.querySelector(".card__like-button");
+  const deleteBtn = cardElement.querySelector(".card__delete-button");
+
+  likeBtn.addEventListener("click", () =>{
+    likeBtn.classList.toggle("card__like-button_active");
+  });
+
+  deleteBtn.addEventListener("click", () =>{
+    deleteBtn.parentElement.remove();
+  });
+
+  cardImageEl.addEventListener("click", () =>{
+    pictureModalImage.src = data.link;
+    pictureModalImage.alt = data.name;
+    pictureModalTitle.textContent = data.name;
+    openModal(pictureModal);
+  });
+
   cardImageEl.src = data.link;
   cardImageEl.alt = data.name;
-  //sets title of card
   cardTitleEl.textContent = data.name;
   return cardElement;
 }
 
-//***Event Handlers
+//***Event Handlers***
 function handleProfileEditSubmit(e){
-  //stops refreshing
   e.preventDefault();
   profileTitle.innerText = profileModalNameInput.value;
   profileDescription.innerText = profileModalDescriptionInput.value;
   closeModal(profileEditModal);
 }
 
+function handleAddCardModalSubmit(e){
+  e.preventDefault();
+  let cardData =
+  {
+    name: cardModalTitleInput.value,
+    link: cardModalImageInput.value,
+  };
+  cardList.insertAdjacentElement("afterbegin", getCardElement(cardData));
+  closeModal(addCardModal);
+}
 
 //***Event Listeners***
-//Brings up the profile editor modal with default values inplace
-profileEditBtn.addEventListener("click", () => {
-  profileModalNameInput.value = profileTitle.innerText;
-  profileModalDescriptionInput.value = profileDescription.innerText;
-  profileEditModal.classList.add("modal_opened");
-});
 
-//Closes the profile editor modal when exit button is pressed
+//Modal Close Buttons
 profileModalCloseBtn.addEventListener("click", () => {
   closeModal(profileEditModal);
 });
+cardModalCloseBtn.addEventListener("click", () =>{
+  closeModal(addCardModal);
+});
+pictureModalCloseBtn.addEventListener("click", () =>{
+  closeModal(pictureModal);
+});
 
-//Saves the profile editor modal when save button is pressed
-//Grabs input values and puts that into the profile
+profileEditBtn.addEventListener("click", () => {
+  profileModalNameInput.value = profileTitle.innerText;
+  profileModalDescriptionInput.value = profileDescription.innerText;
+  openModal(profileEditModal);
+});
+
+addCardBtn.addEventListener("click", () => {
+  openModal(addCardModal);
+});
+
+addCardModal.addEventListener("submit", handleAddCardModalSubmit);
+
 profileEditModal.addEventListener("submit", handleProfileEditSubmit);
 
+//sets initial cards
 initialCards.forEach((cardData) => {
   cardList.insertAdjacentElement("beforeend", getCardElement(cardData));
 });
